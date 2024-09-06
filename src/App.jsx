372bc88cac200ad6b23dfe459bc59eda9aa5b6fe
mohamuhsin <muhsinohoho@outlook.com>
2024-09-06 22:27:49 +0300

    worked on the state management for the travel list project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import Form from "./components/Form";
+import Footer from "./components/Footer";
+import Packing from "./components/Packing";
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItem(item) {
+    setItems((prevItems) => [...prevItems, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
   return (
     <div className="app">
       <Header />
-      <Form />
+      <Form onAddItem={handleAddItem} />
+      <Packing
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
+      <Footer />
     </div>
   );
 }
